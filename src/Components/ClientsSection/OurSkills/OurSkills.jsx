7070a3b626654d16267b6c61a508f0e1ec316058
Alexandr Kazak <alexandr.kazak.v@gmail.com>
2024-08-13 12:45:36 +0300

    changed skill area

--- src/Components/ClientsSection/OurSkills/OurSkills.jsx
+++ src/Components/ClientsSection/OurSkills/OurSkills.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import style from './style.module.scss'
 
-const OurSkills = () => {
+const Card = ({title, text, name, description, media}) => {
 	return (
 		<div className={style.wrapper}>
 			<p className={style.setionTitle}>Some Words From Our Clients </p>
 			<div className={style.cardsContainer}>
-				<div className={style.card}>
-					<div className={style.cardMedia}></div>
-					<div className={style.cardContent}>
-						<div className={style.presentationText}>
+		<div className={style.card}>
+			{media && <div className={style.cardMedia}>{media}</div>}
+			<div className={style.cardContent}>
+				<div className={style.presentationText}>
 							<p className={style.title}>Wonderful Service</p>
 							<p className={style.text}>
 								MyBranch is an office space solution provider in India, founded
